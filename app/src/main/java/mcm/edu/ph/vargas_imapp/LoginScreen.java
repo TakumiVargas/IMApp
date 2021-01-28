64780f9ff4d7b78e6cade04d65e95aeba2ed5ca9
varga <vargastakumi123456789@gmail.com>
2021-01-29 04:09:58 +0800

    commit

---
+++ app/src/main/java/mcm/edu/ph/vargas_imapp/LoginScreen.java
@@ -0,0 +1,13 @@
+package mcm.edu.ph.vargas_imapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_screen);
+    }
+}
